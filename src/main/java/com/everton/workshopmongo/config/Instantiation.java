ca45367a6de39f088a0ec49e4155afecbaabfea5
evertonbernaski <admtradereverton@gmail.com>
2021-04-17 12:42:01 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/everton/workshopmongo/config/Instantiation.java
+++ src/main/java/com/everton/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.everton.workshopmongo.domain.Post;
 import com.everton.workshopmongo.domain.User;
+import com.everton.workshopmongo.dto.AuthorDTO;
 import com.everton.workshopmongo.repository.PostRepository;
 import com.everton.workshopmongo.repository.UserRepository;
 
@@ -35,12 +36,13 @@ public void run(String... args) throws Exception {
 	User alex = new User(null, "Alex Green", "alex@gmail.com");
 	User bob = new User(null, "Bob Grey", "bob@gmail.com");
 	
+	userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-	Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-	Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+	Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",new AuthorDTO(maria));
+	Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!",new AuthorDTO(maria));
 
 	
-	userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	
 	postRepository.saveAll(Arrays.asList(post1, post2));
 }
 }

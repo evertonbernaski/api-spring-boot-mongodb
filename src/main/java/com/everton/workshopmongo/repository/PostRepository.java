eaeb01b73e76b74092d6938ead2dda7504e17537
evertonbernaski <admtradereverton@gmail.com>
2021-04-17 12:30:52 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/everton/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.everton.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.everton.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
